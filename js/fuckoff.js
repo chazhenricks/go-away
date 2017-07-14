c9154e2d0bf2f2b344bcdfcc52a6587baab74bd5
Chaz Henricks <chazhenricks@gmail.com>
2017-07-14 09:10:18 -0500

    FUCK OFF

---
+++ js/fuckoff.js
@@ -0,0 +1,40 @@
+var responseDiv = document.getElementById("response");
+var sendRequest = document.getElementById("submit");
+var userName = document.getElementById("name");
+var userNoun = document.getElementById("noun");
+var fuckoff = "https://www.foaas.com";
+
+
+function fuckOffRequest(name, noun) {
+    console.log("URL IS ", `${fuckoff}/greed/${noun}/${name}`);
+    return new Promise(function (resolve, reject){
+        $.ajax({
+            url: `${fuckoff}/greed/${noun}/${name}`
+        }).done(function(data){
+            resolve(data);
+        }).fail(function(error){
+            reject(error);
+        });
+    });
+};
+
+
+
+
+sendRequest.addEventListener("click", function(){
+
+    fuckOffRequest(userName.value, userNoun.value)
+    .then(function(response){
+        showData(response);
+    });
+});
+
+
+
+
+function showData(data){
+    var fuckoffResponse = "<p>";
+    fuckoffResponse += data;
+    fuckoffResponse += "</p>";
+    response.innerHTML = fuckoffResponse;
+};
